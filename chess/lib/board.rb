0ea739871cedf97c58e1609b985651bf708c4f12
samuellmoney <SLMSoftware@outlook.com>
2019-04-23 00:28:39 +0700

    Adding preliminary piece files

---
+++ chess/lib/board.rb
@@ -0,0 +1,37 @@
+relative_require("./piece.rb")
+
+class Board
+
+  @@boardPositions
+
+  #initialize board
+  def initialize()
+    @@boardPositions = setup_board
+  end
+
+  def move_piece(start_pos, end_pos)
+    raise RuntimeError unless @@boardPosition[start_pos] != nil
+    raise RuntimeError unless @@boardPosition[end_pos] == nil
+    @@boardPositions[start_pos], @@boardPositions[end_pos] = @@boardPositions
+  end
+
+
+  Private
+  @@spacesInRow = 8
+
+  def setup_board()
+    tempBoardPositions = [@@spacesInRow]
+    pieceRows = [0,1,6,7]
+    (0...@@spacesInRow).each do |idx|
+      newRowArr = []
+      if pieceRows.includes?(idx)
+        @@spacesInRow.times { newRowArr << Piece.new }
+      else
+        @@spacesInRow.times { newRowArr << nil }
+      end
+      tempBoardPositions[idx] << newRowArr
+    end
+    tempBoardPositions
+  end
+
+end
